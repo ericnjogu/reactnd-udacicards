6ef78196abf08b80ecd039edbc1f8f4fd3aea696
Eric Njogu <kunadawa@gmail.com>
2019-05-14 18:58:30 +0300

    feat: adding decks component displaying dummy data

---
+++ components/Decks.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native';
+import {AntDesign} from '@expo/vector-icons'
+import {gray} from '../utils/colors'
+
+
+export default class Decks extends React.Component {
+	getDeckList = () => {
+		// TODO replace with async storage call
+		return testDecks
+	}
+
+	showDeck = (deckId) => {
+		// TODO navigate to deck component
+	}
+
+	render() {
+		const decks = this.getDeckList()
+		return (
+			<View>
+				<Text style={{marginTop:20, fontSize:30}}>Decks</Text>
+				<FlatList 
+					data={Object.keys(decks).map(deckId => decks[deckId])}
+					renderItem={
+						({item}) => 
+							<TouchableOpacity onPress={() => this.showDeck(item)}>
+								<Text style={styles.deck}>{`${item.title} - ${Object.keys(item.questions).length} card(s)`}</Text>
+							</TouchableOpacity>
+					}
+					keyExtractor={(item, index) => item.title}
+				/>
+				<TouchableOpacity onPress={this.addDeck} style={{marginTop:50, alignItems:'center'}} >
+					<AntDesign name='plussquareo' size={60}/>
+					<Text style={{fontSize:20}}>Add Deck</Text>
+				</TouchableOpacity>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	deck:{
+		fontSize:20,
+		flex:1,
+		backgroundColor:gray,
+		margin:10
+	}
+})
+
+const testDecks = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
